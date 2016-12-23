a905d2d6c0737942ccc9974e55d5f0ab113e0e56
Casey Chu <casey@bitsofpancake.com>
2016-12-23 01:03:52 -0800

    Serve static files, including Katex

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const open = require('open');
+const path = require('path');
 const retry = require('retry');
 const url = require('url');
 const ws = require('ws');
@@ -11,33 +12,28 @@ const NotexReact = require('./notex-react.js');
 
 const file = process.argv[2];
 const server = http.createServer(function (req, res) {
+	const pathname = url.parse(req.url).pathname;
 	
-	switch (url.parse(req.url).pathname) {
-		case '/':
-			read().then(function (contents) {
-				res.writeHead(200, { 'Content-type': 'text/html' });
-				res.end(renderHTML(contents));
-			});
-			return;
-		
-		case '/script.js':
-			fs.readFile('./client/script.js', function (err, contents) {
-				res.writeHead(200, { 'Content-type': 'text/javascript' });
-				res.end(contents.toString('utf8'));
-			});
-			return;
-		case '/style.css':
-			fs.readFile('./client/style.css', function (err, contents) {
-				res.writeHead(200, { 'Content-type': 'text/css' });
-				res.end(contents.toString('utf8'));
-			});
-			return;
-		
-		default:
-			res.writeHead(404);
-			res.end('Not found');
-			return;
+	if (pathname === '/') {
+		read().then(function (contents) {
+			res.writeHead(200, { 'Content-type': 'text/html' });
+			res.end(renderHTML(contents));
+		});
+		return;
 	}
+	
+	if (pathname.indexOf('/client/') === 0) {
+		serveStatic('./client', pathname.substr('/client/'.length), req, res);
+		return;
+	}
+	
+	if (pathname.indexOf('/katex/') === 0) {
+		serveStatic(path.join(path.dirname(require.resolve('katex')), 'dist'), pathname.substr('/katex/'.length), req, res);
+		return;
+	}
+	
+	res.writeHead(404);
+	res.end('Not found');
 });
 
 const wss = new ws.Server({ server });
@@ -52,6 +48,36 @@ fs.watch(file, function () {
 server.listen(0, function () {
 	open('http://localhost:' + server.address().port);
 });
+
+const mimeTypes = {
+	'.css': 'text/css',
+	'.js': 'text/javascript',
+	'.eot': 'application/vnd.ms-fontobject',
+	'.ttf': 'application/x-font-ttf',
+	'.woff': 'application/font-woff',
+	'.woff2': 'font/woff2'
+};
+
+function serveStatic(root, pathname, req, res) {
+	root = path.resolve(root);
+	const filename = path.resolve(path.join(root, pathname));
+	
+	if (filename.indexOf(root) !== 0) {	
+		res.writeHead(403);
+		res.end('Forbidden');
+		return;
+	}
+	
+	const stream = fs.createReadStream(filename);
+	stream.once('error', function () {
+		res.writeHead(404);
+		res.end('Not found');
+	});
+	stream.once('readable', function () {
+		res.writeHead(200, { 'Content-type': mimeTypes[path.extname(filename)] });
+		stream.pipe(res);
+	});
+}
 
 // Time out.
 setInterval(function () {
@@ -84,12 +110,12 @@ const preamble = `<!DOCTYPE html>
 <head>
 	<meta charset="UTF-8" />
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css" />
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="/katex/katex.min.css" />
+	<link rel="stylesheet" type="text/css" href="/client/style.css" />
 	</head>
 	<body>`;
 	
 const postscript = `
-		<script src="script.js"></script>
+		<script src="/client/script.js"></script>
 	</body>
 </html>`;
