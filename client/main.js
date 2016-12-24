7d8157a21ea3aa764ca98bbdbee81519dbe613f6
Casey Chu <casey@bitsofpancake.com>
2016-12-23 17:26:40 -0800

    Temporary hack to display title

--- client/main.js
+++ client/main.js
@@ -4,4 +4,10 @@ const NotexReact = require('../notex-react.js');
 const socket = new WebSocket('ws://' + location.host);
 socket.onmessage = function (event) {
 	ReactDOM.render(NotexReact.render(JSON.parse(event.data)), document.body);
+	window.onload();
+};
+
+window.onload = function () {
+	const h1 = document.querySelector('.h1 > .inline');
+	document.title = h1 ? h1.innerText : 'Untitled';
 };
