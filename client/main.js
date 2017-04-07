6ad26f70a7c0854fe3e39ae1f179fac08b1d7dd5
Casey Chu <casey@bitsofpancake.com>
2017-04-06 22:45:03 -0700

    autoprint

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ const ReactDOM = require('react-dom');
 const NotexReact = require('../notex-react.js');
 
 window.render = function render(doc) {
-	// If we're already at the bottom, scroll to the bottom.
+	// If we're already at the bottom, scroll to the bottom after rerendering.
 	// Possibly non-cross-browser!
 	var atBottom = window.scrollY + document.documentElement.clientHeight === document.documentElement.scrollHeight;
 
@@ -20,4 +20,17 @@ window.render = function render(doc) {
 const socket = new WebSocket('ws://' + location.host);
 socket.onmessage = function (event) {
 	render(JSON.parse(event.data));
+};
+
+window.onload = function () {
+	if (window.location.search.indexOf('autoprint') !== -1) {
+		// Close the window after printing.
+		if (window.matchMedia)
+			window.matchMedia('print').addListener(function (media) {
+				if (!media.matches)
+					window.close();
+			});
+			
+		window.print();
+	}
 };
